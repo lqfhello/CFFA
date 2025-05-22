a6dceaa4246e72374e08ebc7428b6c505640a37b
lqf <1364973275@qq.com>
2025-05-22 18:53:45 +0800

    first commit

--- main.py
+++ main.py
@@ -7,6 +7,6 @@
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("hello")
+    print("helloworld")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
